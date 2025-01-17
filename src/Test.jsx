ffeaf06adbda946d2787da7aa793af0b5ac7e319
saurashya <saurashya1@gmail.com>
2025-01-17 12:48:52 +0545

    Updated Coming soon Page .

--- src/Test.jsx
+++ src/Test.jsx
@@ -6,19 +6,19 @@ import FluidCursor from './Components/FluidCursor';
 const Test = () => {
 
   return (
-   <>
+   <div className='cursor-none'>
     <FluidCursor/>
-     <div className='bg-gradient-to-br from-purple-800 from-5% via-black via-50% to-indigo-600 to-80% h-[100vh] w-[100vw] flex justify-center items-center  '>
+     <div className='bg-gradient-to-br from-purple-800 from-5% via-black via-50% to-indigo-600 to-80% h-[100vh] w-[100vw] flex justify-center items-center'>
         <div className="container-timer flex justify-start items-start z-30 sm:justify-center sm:items-center">
                 <div className="container-text-timer h-[50%] w-[100%] relative cursor-default">
                     <div className="image h-[100%] w-[100%]">
-                        <img src={vision} alt="" />
+                        <img src={vision} alt=""  className='cursor-none'/>
                     </div>
-                    <h3 className='text-white text-sm absolute top-1/2 left-[55%] -translate-x-1/2 -translate-y-1/2 w-[80%] sm:text-2xl'>CODE YATRA COMING SOON ! STAY TUNED !</h3>
+                    <h3 className='text-white text-sm absolute top-1/2 left-[53%] -translate-x-1/2 -translate-y-1/2 w-[80%] sm:text-2xl cursor-none merriweather-regular'>CODE YATRA COMING SOON ! STAY TUNED !</h3>
                 </div>
             </div>
     </div>
-   </>
+   </div>
   );
 };
 
