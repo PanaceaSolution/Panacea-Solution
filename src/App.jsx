df77b03daf91d8e9a203aaaa8ce2f76c0538a76b
saurashya <saurashya1@gmail.com>
2025-02-05 14:58:07 +0545

    Bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { BrowserRouter,Routes,Route} from 'react-router-dom'
 import Home from './Pages/Home'
-import Test from "./Pages/Test"
 import CodeOfConduct from './Pages/CodeOfConduct'
 
 const App = () => {
