cefbfee2711310520cab5547e4c1def79bcb42e0
saurashya <saurashya1@gmail.com>
2025-03-19 21:32:17 +0545

    Fixing vercel routing

--- src/Components/ServicesCards.jsx
+++ src/Components/ServicesCards.jsx
@@ -7,6 +7,11 @@ import App from '/assets/app.png';
 import Ui from '/assets/ui.png';
 import Digital from '/assets/digital.png';
 import Ecommerce from '/assets/ecommerce.png'
+import card1 from '/assets/card1.png';
+import card2 from '/assets/card2.png';
+import card3 from '/assets/card3.png';
+import card4 from '/assets/card4.png';
+import card5 from '/assets/card5.png';
 
 
 const ServicesCards = () => {
@@ -25,35 +30,35 @@ const ServicesCards = () => {
             content = <ServiceContent 
                 heading={"Web Development"} 
                 content={"We specialize in creating responsive, high-performance websites that provide a seamless user experience across all devices."} 
-                source={panacea} 
+                source={card1} 
             />;
             break;
         case 2:
             content = <ServiceContent 
                 heading={"App Development"} 
                 content={"We build robust mobile applications that offer intuitive interfaces and smooth functionality for both iOS and Android platforms."} 
-                source={panacea} 
+                source={card2} 
             />;
             break;
         case 3:
             content = <ServiceContent 
                 heading={"UI/UX Design"} 
                 content={"Our design team focuses on crafting beautiful, user-friendly interfaces and experiences that engage your users and promote your brand."} 
-                source={panacea} 
+                source={card3} 
             />;
             break;
         case 4:
             content = <ServiceContent 
                 heading={"Digital Marketing"} 
                 content={"We help you amplify your online presence with tailored strategies, leveraging SEO, social media, and content marketing to drive traffic and conversions."} 
-                source={panacea} 
+                source={card4} 
             />;
             break;
         case 5:
             content = <ServiceContent 
                 heading={"Ecommerce Solutions"} 
                 content={"We offer end-to-end eCommerce solutions, from platform development to integrated payment systems, ensuring smooth and scalable online stores."} 
-                source={panacea} 
+                source={card5} 
             />;
             break;
         default:
@@ -66,28 +71,28 @@ const ServicesCards = () => {
 
     return (
         <div className='h-[70vh] grid grid-cols-1 md:grid-cols-3 p-12 relative z-[9999] cursor-pointer'>
-            <div className="selectors text-white col-span-1 relative z-[9999] p-4 flex flex-col gap-6">
-                <div className="option relative z-[9999] flex gap-6 items-center cursor-pointer justify-start">
+            <div className="selectors text-white col-span-1 relative z-[9999] p-4 md:flex md:flex-col grid grid-cols-6 gap-6">
+                <div className="option relative z-[9999] flex flex-col gap-6 items-center cursor-pointer justify-start md:flex-row shadow-xl hover:bg-[#EFF6FF]">
                     <img src={IT} alt="Panacea" className='h-[5vh] relative z-[9999]' onClick={() => setSelect(0)}/>
                     <h3 onClick={() => setSelect(0)} className='text-center text-sm md:text-lg font-medium'>IT</h3>
                 </div>
-                <div className="option relative z-[9999] flex gap-6 items-center cursor-pointer justify-start">
+                <div className="option relative z-[9999] flex flex-col gap-6 items-center cursor-pointer justify-start md:flex-row">
                     <img src={Web} alt="Panacea" className='h-[5vh] relative z-[9999]' onClick={() => setSelect(1)}/>
                     <h3 onClick={() => setSelect(1)} className='text-center text-sm md:text-lg uppercase'>Web development</h3>
                 </div>
-                <div className="option relative z-[9999] flex gap-6 items-center cursor-pointer justify-start">
+                <div className="option relative z-[9999] flex flex-col gap-6 items-center cursor-pointer justify-start md:flex-row">
                     <img src={App} alt="Panacea" className='h-[5vh] relative z-[9999]' onClick={() => setSelect(2)}/>
                     <h3 onClick={() => setSelect(2)} className='text-center text-sm md:text-lg uppercase'>App development</h3>
                 </div>
-                <div className="option relative z-[9999] flex gap-6 items-center cursor-pointer justify-start">
+                <div className="option relative z-[9999] flex flex-col gap-6 items-center cursor-pointer justify-start md:flex-row">
                     <img src={Ui} alt="Panacea" className='h-[5vh] relative z-[9999]' onClick={() => setSelect(3)}/>
                     <h3 onClick={() => setSelect(3)} className='text-center text-sm md:text-lg uppercase'>UI/UX design</h3>
                 </div>
-                <div className="option relative z-[9999] flex gap-6 items-center cursor-pointer justify-start">
+                <div className="option relative z-[9999] flex flex-col gap-6 items-center cursor-pointer justify-start md:flex-row">
                     <img src={Digital} alt="Panacea" className='h-[5vh] relative z-[9999]' onClick={() => setSelect(4)}/>
                     <h3 onClick={() => setSelect(4)} className='text-center text-sm md:text-lg uppercase'>Digital Marketing</h3>
                 </div>
-                <div className="option relative z-[9999] flex gap-6 items-center cursor-pointer justify-start">
+                <div className="option relative z-[9999] flex flex-col gap-6 items-center cursor-pointer justify-start md:flex-row">
                     <img src={Ecommerce} alt="Panacea" className='h-[5vh] relative z-[9999]' onClick={() => setSelect(5)}/>
                     <h3 onClick={() => setSelect(5)} className='text-center text-sm md:text-lg uppercase'>Ecommerce Solution</h3>
                 </div>
--- src/Pages/Test.jsx
+++ src/Pages/Test.jsx
@@ -1,29 +1,9 @@
-import React, { useState } from 'react'
-import ExpandingCards from '../Components/ExpandingCards';
+import React from 'react'
 
 const Test = () => {
-    const [card1,setCard1]=useState(false);
-    const handleHover1=()=>{
-        setCard1((state)=>!state);
-    }
-    const FirstCardActive =()=>{
-        return(
-            <div onMouseLeave={handleHover1} className='bg-white h-[40%] w-[30%] rounded-lg p-2 cursor-pointer transition-all duration-300 ease-in-out'>
-                <p className='text-black'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, accusamus! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis, possimus!</p>
-            </div>
-        )
-    }
-    const FirstCard =()=>{
-        return(
-            <div onMouseOver={handleHover1} className= {`bg-white h-[40%] w-[10%] rounded-lg p-2 cursor-pointer`}>
-                <p className='text-black'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, iusto?</p>
-            </div>
-        )
-    }
   return (
-    <div className='flex flex-col justify-center items-center h-[50vh] w-[100vw] gap-6 text-white cursor-default bg-black'>
-       <ExpandingCards content={"lorem ipsum"} contentActive={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, qui?"}/>
-        </div>
+    <div>Test</div>
   )
 }
+
 export default Test
