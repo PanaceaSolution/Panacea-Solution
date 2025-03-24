73aeb92e989c35798e694e8b086b9a7757624adc
saurashya <saurashya1@gmail.com>
2025-03-24 14:23:00 +0545

    Rebased

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -13,7 +13,7 @@ const Footer = lazy(() => import('./Footer'));
 
 const Home = () => {
   return (
-    <div className='bg-gradient-to-br from-[#000a29] from-5% via-[#002f69] via-50% to-[#429ebd] to-80% cursor-none' id='container'>
+    <div className='bg-gradient-to-br from-[#000a29] from-5% via-[#002f69] via-50% to-[#429ebd] to-80% cursor-none overflow-x-hidden' id='container'>
       <FluidCursor />
       <Suspense fallback={<Loading/>}>
         <NavBar />
