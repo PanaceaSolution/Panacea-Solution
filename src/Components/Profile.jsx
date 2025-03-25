593717bd4a8e7ab8b5460fbfe593adeaebf8ab68
saurashya <saurashya1@gmail.com>
2025-03-25 12:08:06 +0545

    Fixed sizes and scroll bug

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Profile = ({name,position,source}) => {
   return (
     <div className='flex flex-col gap-4 text-white p-2 py-8'>
-        <img src={source} className='h-[10vh] w-[10vh] rounded-full object-cover sm:h-[20vh] sm:w-[20vh] object-center z-20 cursor-pointer' draggable="false"/>
+        <img src={source} className='h-[10vh] w-[10vh] rounded-full object-cover sm:h-[15vh] sm:w-[15vh] object-center z-20 cursor-pointer' draggable="false"/>
         <h1 className='text-lg font-semibold sm:text-xl'>{name}</h1>
         <h1 className='text-sm sm:text-lg'>{position}</h1>
     </div>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -13,7 +13,7 @@ const Footer = lazy(() => import('./Footer'));
 
 const Home = () => {
   return (
-    <div className='bg-gradient-to-br from-[#000a29] from-5% via-[#002f69] via-50% to-[#429ebd] to-80% cursor-none' id='container'>
+    <div className='bg-gradient-to-br from-[#000a29] from-5% via-[#002f69] via-50% to-[#429ebd] to-80% cursor-none w-[100vw] h-auto overflow-x-hidden' id='container'>
       <FluidCursor />
       <Suspense fallback={<Loading/>}>
         <NavBar />
@@ -21,9 +21,9 @@ const Home = () => {
         <AboutCompany />
         <Services />
         <Team />
-        {/* <StandOut/>
+        <StandOut/>
         <Testimonials/>
-        <Footer /> */}
+        <Footer />
       </Suspense>
     </div>
   );
